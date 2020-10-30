1c6769eff2ca0037c84facf0f8ec55d914b7e67d
Rafael <rortizr@unbosque.edu.co>
2020-10-30 13:16:55 -0500

    Unidad 1

---
+++ Unidad_1/Actividad_2/Actividad1UI/script.js
@@ -0,0 +1,4 @@
+function mensajeArtista() {
+    var artista = document.getElementById('artista').value;
+    alert("Este artista es " + artista);
+}
---
+++ Unidad_1/Actividad_3/js/script.js
@@ -0,0 +1,4 @@
+function enviarDatos() {
+    let nombre = document.getElementById('nombre').value;
+    alert('Gracias ' + nombre + ' por ponerte en contacto con nosotros');
+}
